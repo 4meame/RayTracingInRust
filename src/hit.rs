1709f4a9549796841edaa76ba8a7cc0ae25b006b
Amethyst <lizujun@xmfunny.com>
2022-12-21 18:56:41 +0800

    Update Parallelism

--- src/hit.rs
+++ src/hit.rs
@@ -1,22 +1,22 @@
-use std::rc::Rc;
 use super::vec::{Vec3, Point3};
 use super::ray::Ray;
 use super::mat::Material;
 
-pub struct HitRecord {
+pub struct HitRecord<'a> {
     pub position: Point3,
     pub normal: Vec3,
     pub t: f64,
     pub front_face: bool,
     // explaination: the reference counted smart pointer, Rc<T> type keeps track of the number of references to a value to determine whether or not the value is still in use
-    pub material: Rc<dyn Material>
+    // pub material: Rc<dyn Material>
+    pub material: &'a dyn Material
 }
 
-pub trait Hit {
+pub trait Hit: Sync {
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
 }
 
-impl HitRecord {
+impl HitRecord<'_> {
     pub fn set_face_normal(&mut self, r: &Ray, outward_normal: Vec3) ->() {
         self.front_face = r.direction().dot(outward_normal) < 0.0;
         self.normal = if self.front_face {
